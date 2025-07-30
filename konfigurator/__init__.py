d2ba7a5474a892aa0c8d69956d9782f66419cfc0
Maxim Dolgov <maxim.dolgov@hs-pforzheim.de>
2025-07-30 10:11:41 +0000

    Introduce nested config and dependent overrides (#4)

--- konfigurator/__init__.py
+++ konfigurator/__init__.py
@@ -1,9 +1,9 @@
 from .utils import (  # noqa: F401
+    FieldReference,
     class_from_path,
     get_class_path,
     instantiate_object_from_config,
     load_config,
-    update_config_with_overrides,
 )
 
 __all__ = [
@@ -11,5 +11,5 @@ __all__ = [
     "class_from_path",
     "instantiate_object_from_config",
     "load_config",
-    "update_config_with_overrides",
+    "FieldReference",
 ]
