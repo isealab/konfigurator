388c8189dac1c8ab3e36c2cc51a7999656370058
Maxim Dolgov <maxim.dolgov@hs-pforzheim.de>
2025-07-30 12:10:36 +0200

    Remove private function from exposure

--- konfigurator/__init__.py
+++ konfigurator/__init__.py
@@ -1,6 +1,5 @@
 from .utils import (  # noqa: F401
     FieldReference,
-    _update_config_with_overrides,
     class_from_path,
     get_class_path,
     instantiate_object_from_config,
@@ -12,6 +11,5 @@ __all__ = [
     "class_from_path",
     "instantiate_object_from_config",
     "load_config",
-    "_update_config_with_overrides",
     "FieldReference",
 ]
